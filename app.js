b518c8bc24119733ba03bc0c31b1da6f9792a224
unknown <sjc1b@hotmail.es>
2021-04-10 00:25:18 +0200

    fix: cors errors

--- app.js
+++ app.js
@@ -20,19 +20,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-app.use('/posts', postRouter);
+
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }, {useUnifiedTopology:true})
     .then(() =>  console.log('mymerndb connection successful'))
     .catch((err) => console.error(err));
 
-app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -40,8 +36,15 @@ app.use(function(req, res, next) {
   next();
   });
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" })); 
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); 
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/posts', postRouter);
 
 
 // catch 404 and forward to error handler
@@ -50,14 +53,13 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
 module.exports = app;
